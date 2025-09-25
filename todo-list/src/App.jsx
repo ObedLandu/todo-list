dc528b093a0dde78ac9d806d52c895d6e02f7c7a
ObedLandu <obedlandu08@gmail.com>
2025-09-25 14:50:42 +0100

    finale project

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Task from "./components/Task/Task";
 
@@ -7,9 +7,26 @@ export default function App() {
   const [tasks, setTasks] = useState([
     { content: "première tâche", isDone: false },
   ]);
-  //fonction pour ajouter une tache
+  //Ref
+  const input = useRef("");
+
+  //cycle
+  useEffect(() => {
+    input.current.focus();
+  });
 
   //fonctions
+  const submittedHandler = (e) => {
+    e.preventDefault();
+    console.log(input.current.value);
+    const newTask = {
+      content: input.current.value,
+      isDone: false,
+    };
+    setTasks([newTask, ...tasks]);
+    input.current.value = "";
+  };
+
   const isDoneCliquedHandler = (index) => {
     console.log("click");
     const newTasks = [...tasks];
@@ -31,8 +48,12 @@ export default function App() {
       </header>
 
       <div className="add">
-        <form>
-          <input type="text" placeholder="Que souhaitez-vous ajouter ?" />
+        <form onSubmit={(e) => submittedHandler(e)}>
+          <input
+            type="text"
+            placeholder="Que souhaitez-vous ajouter ?"
+            ref={input}
+          />
           <button type="submit">Ajouter</button>
         </form>
       </div>
